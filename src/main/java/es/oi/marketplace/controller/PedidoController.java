6a61b6352dacf36ae7730fd1c552ed3dd90ecb0d
xhj009 <rafa_maya@outlook.es>
2021-11-23 02:54:46 +0100

    Añado ruta

--- src/main/java/es/oi/marketplace/controller/PedidoController.java
+++ src/main/java/es/oi/marketplace/controller/PedidoController.java
@@ -1,5 +1,7 @@
 package es.oi.marketplace.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.oi.marketplace.dto.Articulodto;
 import es.oi.marketplace.dto.Pedidodto;
 import es.oi.marketplace.service.PedidoService;
 
@@ -37,9 +40,15 @@ public class PedidoController {
 		return new ResponseEntity<String>(pedidoService.actualizarPedido(id, pedidodto));
 	}
 	
+	
 	@DeleteMapping(path =  "/{id}")
 	public void eliminarPedido(@PathVariable int id) {
 		pedidoService.delete(id);
+	}
+	
+	@GetMapping(path = "/nombre/{nombreparcial}")
+	public ResponseEntity<List<Pedidodto>> pedidosQueContengan(@PathVariable String nombreparcial ){
+		return new ResponseEntity<List<Pedidodto>>(pedidoService.buscarPedidosQueContengan(nombreparcial),HttpStatus.ACCEPTED);
 	}
 	
 }
